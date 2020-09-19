c4fc9b21e6126233fd328d6231f04607975020c9
JiayinCao <caojiayin1985@gmail.com>
2020-09-19 10:37:52 -0700

    [EmptyWindow] Functional c++ version vulkan implementation
    - There is still something to be done before wrapping up
    - Cleaning the resources
    - Properly handle corner case errors
    - Remove c implementation

--- Src/Basic/1-EmptyWindow/vulkan/vulkan_impl.cpp
+++ Src/Basic/1-EmptyWindow/vulkan/vulkan_impl.cpp
@@ -3,16 +3,23 @@
 //  Copyright(c) 2020 - 2020 by Jiayin Cao - All rights reserved.
 //
 
+#define DISABLE_VULKAN_C    1
 #include <vector>
 #include <windows.h>
 #include <memory>
-#include "vulkan/vulkan.h"
 #include "vulkan_impl.h"
+
+#if !DISABLE_VULKAN_C
+#include "vulkan/vulkan.h"
+#endif
 
 #define VULKAN_HPP_NO_EXCEPTIONS
 #define VULKAN_HPP_TYPESAFE_CONVERSION
 #include <vulkan/vulkan.hpp>
 #include <vulkan/vk_sdk_platform.h>
+
+// Allow a maximum of two outstanding presentation operations.
+#define FRAME_LAG 2
 
 #define VERIFY(ret)         if(ret != vk::Result::eSuccess) return false;
 
@@ -37,8 +44,50 @@ vk::SurfaceKHR                                  g_vk_surface;
 // graphics queue and present queue
 vk::Queue                                       g_vk_graphics_queue;
 vk::Queue                                       g_vk_present_queue;
-// vulkan device
+// Vulkan device
 vk::Device                                      g_vk_device;
+// Vulkan surface format
+vk::Format                                      g_vk_surface_format;
+// Vulkan color space
+vk::ColorSpaceKHR                               g_vk_color_space;
+// Vulkan fences
+vk::Fence                                       g_vk_fence[FRAME_LAG];
+// Vulkan semaphores
+vk::Semaphore                                   g_vk_image_acquired_semaphores[FRAME_LAG];
+vk::Semaphore                                   g_vk_draw_complete_semaphores[FRAME_LAG];
+vk::Semaphore                                   g_vk_image_ownership_semaphores[FRAME_LAG];
+// Whether there are separate graphics and present queue
+bool                                            g_vk_separate_queue;
+// Current frame index
+unsigned int                                    g_vk_frame_index;
+
+// Vulkan queue index
+unsigned int                                    g_vk_graphics_queue_family_index = UINT32_MAX;
+unsigned int                                    g_vk_present_queue_family_index = UINT32_MAX;
+
+// Vulkan command pool
+vk::CommandPool                                 g_graphics_cmd_pool;
+vk::CommandPool                                 g_present_cmd_pool;
+
+// Vulkan command list
+std::vector<vk::CommandBuffer>                  g_graphics_cmd;
+
+// Vulkan swap chain
+vk::SwapchainKHR                                g_vk_swapchain;
+
+// current window size
+uint32_t                                        g_width = 0;
+uint32_t                                        g_height = 0;
+
+// final swap chain image count
+uint32_t                                        g_swapchain_image_cnt = 0;
+
+// vulkan image in swap chain
+std::vector<vk::Image>                          g_images;
+// vulkan image view in swap chain
+std::vector<vk::ImageView>                      g_image_views;
+
+#if !DISABLE_VULKAN_C
 
 // This is the vulkan instance
 VkInstance              g_instance;
@@ -107,6 +156,7 @@ static VkResult init_global_extension_properties(layer_properties& layer_props) 
 
     return res;
 }
+#endif
 
 static bool check_layers(uint32_t check_count, char const* const* const check_names, uint32_t layer_count, vk::LayerProperties* layers) {
     for (uint32_t i = 0; i < check_count; i++) {
@@ -263,38 +313,35 @@ static bool create_swapchain(const HINSTANCE hInstnace, const HWND hwnd) {
 
     // Iterate over each queue to learn whether it supports presenting:
     auto supportsPresent = std::make_unique<vk::Bool32[]>(g_vk_queue_family_count);
-    for (uint32_t i = 0; i < g_vk_queue_family_count; i++) {
-        g_vk_physical_device.getSurfaceSupportKHR(i, g_surface, &supportsPresent[i]);
-    }
+    for (uint32_t i = 0; i < g_vk_queue_family_count; i++)
+        g_vk_physical_device.getSurfaceSupportKHR(i, g_vk_surface, &supportsPresent[i]);
 
-    uint32_t graphics_queue_family_index = UINT32_MAX;
-    uint32_t present_queue_family_index = UINT32_MAX;
     for (uint32_t i = 0; i < g_vk_queue_family_count; i++) {
         if (g_vk_queue_properties[i].queueFlags & vk::QueueFlagBits::eGraphics) {
-            if (graphics_queue_family_index == UINT32_MAX) {
-                graphics_queue_family_index = i;
+            if (g_vk_graphics_queue_family_index == UINT32_MAX) {
+                g_vk_graphics_queue_family_index = i;
             }
 
             if (supportsPresent[i] == VK_TRUE) {
-                present_queue_family_index = i;
-                present_queue_family_index = i;
+                g_vk_present_queue_family_index = i;
+                g_vk_present_queue_family_index = i;
                 break;
             }
         }
     }
 
-    if (present_queue_family_index == UINT32_MAX) {
+    if (g_vk_present_queue_family_index == UINT32_MAX) {
         // If didn't find a queue that supports both graphics and present, then find a separate present queue.
         for (uint32_t i = 0; i < g_vk_queue_family_count; ++i) {
             if (supportsPresent[i] == VK_TRUE) {
-                present_queue_family_index = i;
+                g_vk_present_queue_family_index = i;
                 break;
             }
         }
     }
 
     // Generate error if could not find both a graphics and a present queue
-    if (graphics_queue_family_index == UINT32_MAX || present_queue_family_index == UINT32_MAX)
+    if (g_vk_graphics_queue_family_index == UINT32_MAX || g_vk_present_queue_family_index == UINT32_MAX)
         return false;
 
     // Create vulkan device
@@ -302,7 +349,7 @@ static bool create_swapchain(const HINSTANCE hInstnace, const HWND hwnd) {
         float const priorities[1] = { 0.0 };
 
         vk::DeviceQueueCreateInfo queues[2];
-        queues[0].setQueueFamilyIndex(graphics_queue_family_index);
+        queues[0].setQueueFamilyIndex(g_vk_graphics_queue_family_index);
         queues[0].setQueueCount(1);
         queues[0].setPQueuePriorities(priorities);
 
@@ -315,8 +362,8 @@ static bool create_swapchain(const HINSTANCE hInstnace, const HWND hwnd) {
             .setPpEnabledExtensionNames((const char* const*)g_vk_device_exts.data())
             .setPEnabledFeatures(nullptr);
 
-        if (graphics_queue_family_index != present_queue_family_index) {
-            queues[1].setQueueFamilyIndex(present_queue_family_index);
+        if (g_vk_graphics_queue_family_index != g_vk_present_queue_family_index) {
+            queues[1].setQueueFamilyIndex(g_vk_present_queue_family_index);
             queues[1].setQueueCount(1);
             queues[1].setPQueuePriorities(priorities);
             deviceInfo.setQueueCreateInfoCount(2);
@@ -326,8 +373,166 @@ static bool create_swapchain(const HINSTANCE hInstnace, const HWND hwnd) {
         VERIFY(result);
     }
 
-    g_vk_device.getQueue(graphics_queue_family_index, 0, &g_vk_graphics_queue);
-    g_vk_device.getQueue(present_queue_family_index, 0, &g_vk_present_queue);
+    g_vk_device.getQueue(g_vk_graphics_queue_family_index, 0, &g_vk_graphics_queue);
+    g_vk_device.getQueue(g_vk_present_queue_family_index, 0, &g_vk_present_queue);
+
+    g_vk_separate_queue = g_vk_graphics_queue_family_index != g_vk_present_queue_family_index;
+
+    // Get the list of VkFormat's that are supported:
+    uint32_t format_count;
+    auto result = g_vk_physical_device.getSurfaceFormatsKHR(g_vk_surface, &format_count, static_cast<vk::SurfaceFormatKHR*>(nullptr));
+    VERIFY(result);
+
+    auto surface_format = std::make_unique<vk::SurfaceFormatKHR[]>(format_count);
+    result = g_vk_physical_device.getSurfaceFormatsKHR(g_vk_surface, &format_count, surface_format.get());
+    VERIFY(result);
+
+    // If the format list includes just one entry of VK_FORMAT_UNDEFINED, the surface has no preferred format.  Otherwise, at least one
+    // supported format will be returned.
+    if (format_count == 1 && surface_format[0].format == vk::Format::eUndefined)
+        g_vk_surface_format = vk::Format::eB8G8R8A8Unorm;
+    else
+        g_vk_surface_format = surface_format[0].format;
+    g_vk_color_space = surface_format[0].colorSpace;
+
+    // Create semaphores to synchronize acquiring presentable buffers before
+    // rendering and waiting for drawing to be complete before presenting
+    auto const semaphoreCreateInfo = vk::SemaphoreCreateInfo();
+
+    // Create fences that we can use to throttle if we get too far ahead of the image presents
+    auto const fence_ci = vk::FenceCreateInfo().setFlags(vk::FenceCreateFlagBits::eSignaled);
+    for (uint32_t i = 0; i < FRAME_LAG; i++) {
+        result = g_vk_device.createFence(&fence_ci, nullptr, &g_vk_fence[i]);
+        VERIFY(result);
+
+        result = g_vk_device.createSemaphore(&semaphoreCreateInfo, nullptr, &g_vk_image_acquired_semaphores[i]);
+        VERIFY(result);
+
+        result = g_vk_device.createSemaphore(&semaphoreCreateInfo, nullptr, &g_vk_draw_complete_semaphores[i]);
+        VERIFY(result);
+
+        if (g_vk_separate_queue) {
+            result = g_vk_device.createSemaphore(&semaphoreCreateInfo, nullptr, &g_vk_image_ownership_semaphores[i]);
+            VERIFY(result);
+        }
+    }
+    g_vk_frame_index = 0;
+
+    return true;
+}
+
+static bool create_buffers() {
+    vk::SwapchainKHR oldSwapchain = g_vk_swapchain;
+
+    // Check the surface capabilities and formats
+    vk::SurfaceCapabilitiesKHR surf_caps;
+    auto result = g_vk_physical_device.getSurfaceCapabilitiesKHR(g_vk_surface, &surf_caps);
+    VERIFY(result);
+
+    uint32_t present_mode_count;
+    result = g_vk_physical_device.getSurfacePresentModesKHR(g_vk_surface, &present_mode_count, static_cast<vk::PresentModeKHR*>(nullptr));
+    VERIFY(result);
+
+    auto present_modes = std::make_unique<vk::PresentModeKHR[]>(present_mode_count);
+    result = g_vk_physical_device.getSurfacePresentModesKHR(g_vk_surface, &present_mode_count, present_modes.get());
+    VERIFY(result);
+
+    vk::Extent2D swapchainExtent;
+    swapchainExtent = surf_caps.currentExtent;
+
+    g_width = surf_caps.currentExtent.width;
+    g_height = surf_caps.currentExtent.height;
+
+    vk::PresentModeKHR present_mode = vk::PresentModeKHR::eFifo;
+
+    uint32_t swapchain_image_cnt = 3;
+    if (surf_caps.maxImageCount > 0 && swapchain_image_cnt > surf_caps.maxImageCount)
+        swapchain_image_cnt = surf_caps.maxImageCount;
+
+    vk::SurfaceTransformFlagBitsKHR pre_transform;
+    if (surf_caps.supportedTransforms & vk::SurfaceTransformFlagBitsKHR::eIdentity)
+        pre_transform = vk::SurfaceTransformFlagBitsKHR::eIdentity;
+    else
+        pre_transform = surf_caps.currentTransform;
+
+    // Find a supported composite alpha mode - one of these is guaranteed to be set
+    vk::CompositeAlphaFlagBitsKHR compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque;
+    vk::CompositeAlphaFlagBitsKHR compositeAlphaFlags[4] = {
+        vk::CompositeAlphaFlagBitsKHR::eOpaque,
+        vk::CompositeAlphaFlagBitsKHR::ePreMultiplied,
+        vk::CompositeAlphaFlagBitsKHR::ePostMultiplied,
+        vk::CompositeAlphaFlagBitsKHR::eInherit,
+    };
+    for (uint32_t i = 0; i < _countof(compositeAlphaFlags); i++) {
+        if (surf_caps.supportedCompositeAlpha & compositeAlphaFlags[i]) {
+            compositeAlpha = compositeAlphaFlags[i];
+            break;
+        }
+    }
+
+    auto const swapchain_ci = vk::SwapchainCreateInfoKHR()
+        .setSurface(g_vk_surface)
+        .setMinImageCount(swapchain_image_cnt)
+        .setImageFormat(g_vk_surface_format)
+        .setImageColorSpace(g_vk_color_space)
+        .setImageExtent({ swapchainExtent.width, swapchainExtent.height })
+        .setImageArrayLayers(1)
+        .setImageUsage(vk::ImageUsageFlagBits::eColorAttachment)
+        .setImageSharingMode(vk::SharingMode::eExclusive)
+        .setQueueFamilyIndexCount(0)
+        .setPQueueFamilyIndices(nullptr)
+        .setPreTransform(pre_transform)
+        .setCompositeAlpha(compositeAlpha)
+        .setPresentMode(present_mode)
+        .setClipped(true)
+        .setOldSwapchain(oldSwapchain);
+
+    result = g_vk_device.createSwapchainKHR(&swapchain_ci, nullptr, &g_vk_swapchain);
+    VERIFY(result);
+
+    result = g_vk_device.getSwapchainImagesKHR(g_vk_swapchain, &g_swapchain_image_cnt, static_cast<vk::Image*>(nullptr));
+    VERIFY(result);
+
+    // get vulkan images in the swapchain
+    g_images.resize(g_swapchain_image_cnt);
+    result = g_vk_device.getSwapchainImagesKHR(g_vk_swapchain, &g_swapchain_image_cnt, g_images.data());
+    VERIFY(result);
+
+    // get the image views for each image in the swap chain
+    g_image_views.resize(g_swapchain_image_cnt);
+    for (uint32_t i = 0; i < g_swapchain_image_cnt; ++i) {
+        auto color_image_view = vk::ImageViewCreateInfo()
+            .setViewType(vk::ImageViewType::e2D)
+            .setFormat(g_vk_surface_format)
+            .setSubresourceRange(vk::ImageSubresourceRange(vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1));
+
+        color_image_view.image = g_images[i];
+
+        result = g_vk_device.createImageView(&color_image_view, nullptr, &g_image_views[i]);
+        VERIFY(result);
+    }
+
+    return true;
+}
+
+static bool create_command() {
+    auto const cmd_pool_info = vk::CommandPoolCreateInfo().setQueueFamilyIndex(g_vk_graphics_queue_family_index);
+
+    // create the graphics command pool
+    auto result = g_vk_device.createCommandPool(&cmd_pool_info, nullptr, &g_graphics_cmd_pool);
+    VERIFY(result);
+
+    // command buffer for graphics calls
+    auto const cmd = vk::CommandBufferAllocateInfo()
+        .setCommandPool(g_graphics_cmd_pool)
+        .setLevel(vk::CommandBufferLevel::ePrimary)
+        .setCommandBufferCount(1);
+
+    // create the command buffer
+    g_graphics_cmd.resize(g_swapchain_image_cnt);
+    for (uint32_t i = 0; i < g_swapchain_image_cnt; ++i)
+        result = g_vk_device.allocateCommandBuffers(&cmd, &g_graphics_cmd[i]);
+    VERIFY(result);
 
     return true;
 }
@@ -340,11 +545,20 @@ bool VulkanGraphicsSample::initialize(const HINSTANCE hInstnace, const HWND hwnd
     // initialize vulkan device
     if (!create_vk_physical_device())
         return false;
-
-    // create surface
-    if (!create_surface(hInstnace, hwnd))
+    
+    // create swap chain
+    if (!create_swapchain(hInstnace, hwnd))
         return false;
 
+    // create buffers
+    if (!create_buffers())
+        return false;
+
+    // create command pool and commnad list
+    if (!create_command())
+        return false;
+
+#if !DISABLE_VULKAN_C
     VkApplicationInfo       app_info;
     app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;  // sType is mandatory
     app_info.pNext = NULL;                                // pNext is mandatory
@@ -764,11 +978,78 @@ bool VulkanGraphicsSample::initialize(const HINSTANCE hInstnace, const HWND hwnd
     imageAcquiredSemaphoreCreateInfo.flags = 0;
 
     res = vkCreateSemaphore(g_device, &imageAcquiredSemaphoreCreateInfo, NULL, &g_image_acquired_semaphore);
+#endif
 
     return true;
 }
 
 void VulkanGraphicsSample::render_frame() {
+    uint32_t current_buffer = 0;
+
+    g_vk_device.waitForFences(1, &g_vk_fence[g_vk_frame_index], VK_TRUE, UINT64_MAX);
+    g_vk_device.resetFences({ g_vk_fence[g_vk_frame_index] });
+
+    vk::Result result;
+    do {
+        result = g_vk_device.acquireNextImageKHR(g_vk_swapchain, UINT64_MAX, g_vk_image_acquired_semaphores[g_vk_frame_index], vk::Fence(), &current_buffer);
+        if (result == vk::Result::eErrorOutOfDateKHR) {
+            // Normally this should handle the resizing logic. But since the window doesn't support resize, it won't be handled here
+        }
+        else if (result == vk::Result::eSuboptimalKHR) {
+            // swapchain is not as optimal as it could be, but the platform's
+            // presentation engine will still present the image correctly.
+            break;
+        }
+        else if (result == vk::Result::eErrorSurfaceLostKHR) {
+            // to be handled later
+        }
+        else {
+            assert(result == vk::Result::eSuccess);
+        }
+    } while (result != vk::Result::eSuccess);
+
+    {
+        auto const commandInfo = vk::CommandBufferBeginInfo().setFlags(vk::CommandBufferUsageFlagBits::eSimultaneousUse);
+
+        static float g = 0.0f;
+        g += 0.05f;
+
+        vk::DispatchLoaderStatic d;
+        auto result = g_graphics_cmd[current_buffer].begin(&commandInfo);
+        g_graphics_cmd[current_buffer].clearColorImage(g_images[current_buffer], vk::ImageLayout::eGeneral,
+            vk::ClearColorValue(std::array<float, 4>({ {0.4f, 0.6f, sinf(g), 1.0f} })),
+            vk::ImageSubresourceRange(vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1), d);
+
+        g_graphics_cmd[current_buffer].end();
+    }
+
+    vk::PipelineStageFlags const pipe_stage_flags = vk::PipelineStageFlagBits::eColorAttachmentOutput;
+    auto const submit_info = vk::SubmitInfo()
+        .setPWaitDstStageMask(&pipe_stage_flags)
+        .setWaitSemaphoreCount(1)
+        .setPWaitSemaphores(&g_vk_image_acquired_semaphores[g_vk_frame_index])
+        .setCommandBufferCount(1)
+        .setPCommandBuffers(&g_graphics_cmd[current_buffer])
+        .setSignalSemaphoreCount(1)
+        .setPSignalSemaphores(&g_vk_draw_complete_semaphores[g_vk_frame_index]);
+
+    result = g_vk_graphics_queue.submit(1, &submit_info, g_vk_fence[g_vk_frame_index]);
+    assert(result == vk::Result::eSuccess);
+
+    auto const presentInfo = vk::PresentInfoKHR()
+        .setWaitSemaphoreCount(1)
+        .setPWaitSemaphores(g_vk_separate_queue ? &g_vk_image_ownership_semaphores[g_vk_frame_index] : &g_vk_draw_complete_semaphores[g_vk_frame_index])
+        .setSwapchainCount(1)
+        .setPSwapchains(&g_vk_swapchain)
+        .setPImageIndices(&current_buffer);
+
+    result = g_vk_present_queue.presentKHR(&presentInfo);
+    assert(result == vk::Result::eSuccess);
+
+    g_vk_frame_index += 1;
+    g_vk_frame_index %= FRAME_LAG;
+
+#if !DISABLE_VULKAN_C
     // Get the index of the next available swapchain image:
     vkAcquireNextImageKHR(g_device, g_swapchain, UINT64_MAX, g_image_acquired_semaphore, VK_NULL_HANDLE, &g_current_buffer_index);
 
@@ -827,9 +1108,11 @@ void VulkanGraphicsSample::render_frame() {
 
         vkQueuePresentKHR(g_present_queue, &present);
     }
+#endif
 }
 
 void VulkanGraphicsSample::shutdown() {
+#if !DISABLE_VULKAN_C
     for (uint32_t i = 0; i < g_swapchain_image_count; i++)
         vkDestroyImageView(g_device, g_swapchain_buffers[i].view, NULL);
     vkDestroySwapchainKHR(g_device, g_swapchain, NULL);
@@ -844,4 +1127,5 @@ void VulkanGraphicsSample::shutdown() {
 
     // destroy the vulkan instance
     vkDestroyInstance(g_instance, nullptr);
+#endif
 }
